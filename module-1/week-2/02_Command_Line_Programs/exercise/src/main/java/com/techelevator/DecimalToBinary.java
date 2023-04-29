e9e91049f45a4854a6f1c7a663f57a77086181e4
Thomas Her <tomkher@gmail.com>
2023-04-28 21:56:23 -0500

    Saved

--- module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,8 +1,20 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
+		//String decimalValues = input.nextLine();
+
+		//String[] listOfValues = decimalValues.split(" ");
+
+		//for (int i = 0; i <listOfValues.length; i++) {
+		//	listOfValues[i] =
+		//}
+				//Integer.parseInt(value);
 
 	}
 
--- module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
+++ module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
@@ -1,8 +1,27 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter the temperature: ");
+		String value = input.nextLine();
+		int inputTemperature = Integer.parseInt(value);
+
+		System.out.print("Is the temperature in (C)elsius, or (F)ahrenheit? ");
+		String celsiusOrFahrenheit = input.nextLine();
+
+		//for (int c = inputTemperature; c = inputTemperature; c+= 0) {
+			if (celsiusOrFahrenheit == "C") {
+				double temperatureFarenheit = inputTemperature * 1.8 + 32;
+				System.out.print(inputTemperature + "C is " + temperatureFarenheit +"F");
+			} else if (celsiusOrFahrenheit == "F") {
+				double temperatureCelsius = (inputTemperature - 32) / 1.8;
+				System.out.print(inputTemperature + "F is " + temperatureCelsius +"C");
+			}
+		//}
 
 	}
 
