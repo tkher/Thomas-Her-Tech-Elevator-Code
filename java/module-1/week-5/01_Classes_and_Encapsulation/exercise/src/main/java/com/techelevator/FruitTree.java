a9865aa5c68098a27f0a5ce6a1e6ff26e1016c44
Thomas Her <tomkher@gmail.com>
2023-05-18 16:56:21 -0500

    saving point

--- java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/FruitTree.java
+++ java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/FruitTree.java
@@ -1,5 +1,31 @@
 package com.techelevator;
 
 public class FruitTree {
+    //Instance Variables
+    private String typeOfFruit;
+    private int piecesOfFruitLeft;
+//    private int startingPiecesOfFruit;
 
+    //Constructor
+    public FruitTree(String typeOfFruit,int startingPiecesOfFruit) {
+        this.typeOfFruit = typeOfFruit;
+        this.piecesOfFruitLeft = startingPiecesOfFruit;
+    }
+
+    //Getters and Setters
+    public String getTypeOfFruit() { return getTypeOfFruit();}
+
+    public int getPiecesOfFruitLeft() { return getPiecesOfFruitLeft();}
+
+//    public int getStartingPiecesOfFruit() { return startingPiecesOfFruit;}
+
+    //methods
+    public boolean pickFruit (int numberOfPiecesToRemove) {
+        //need more logic
+        if (numberOfPiecesToRemove >= piecesOfFruitLeft) {
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,5 +1,55 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+    //Instance variables
+    private String letterGrade;
+    private String submitterName;
+    private int earnedMarks;
+    private int possibleMarks;
+
+
+    public HomeworkAssignment(int possibleMarks, String submitterName) {
+        this.possibleMarks = possibleMarks;
+        this.submitterName = submitterName;
+    }
+
+//    public HomeworkAssignment() {
+//
+//    }
+
+//Getters and Setters
+    public int getEarnedMarks() {
+        return earnedMarks;
+    }
+
+    public void setEarnedMarks(int earnedMarks) {
+        this.earnedMarks = earnedMarks;
+    }
+
+    public int getPossibleMarks() {
+        return possibleMarks;
+    }
+
+    public String getSubmitterName () {
+        return submitterName;
+    }
+
+    public String getLetterGrade () {
+        double convertedPossibleMarks = this.possibleMarks;
+        double convertedEarnedMarks = this.earnedMarks;
+        double percentage = convertedEarnedMarks / convertedPossibleMarks;
+        if (percentage >= .9) {
+            return "A";
+        } else if (percentage >= .8) {
+            return "B";
+        } else if (percentage >= .7) {
+            return "C";
+        } else if (percentage >= .6) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
 
 }
