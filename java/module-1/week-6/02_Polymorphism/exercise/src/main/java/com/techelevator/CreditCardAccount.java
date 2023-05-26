58378b66b1fabcfcad8dc2885587c11c86abe54d
Thomas Her <tomkher@gmail.com>
2023-05-26 18:02:59 -0500

    save credit card point

---
+++ java/module-1/week-6/02_Polymorphism/exercise/src/main/java/com/techelevator/CreditCardAccount.java
@@ -0,0 +1,45 @@
+package com.techelevator;
+
+public class CreditCardAccount implements Accountable{
+    private String accountHolder;
+    private String accountNumber;
+    private int debt = 0;
+
+    //constructor
+    public CreditCardAccount(String accountHolder, String accountNumber) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+    }
+
+    //getters
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getDebt() {
+        return debt;
+    }
+
+
+    //methods
+    public int pay(int amountToPay) {
+        debt = debt - amountToPay;
+        return debt;
+    }
+
+    public int charge(int amountToCharge) {
+        debt = debt + amountToCharge;
+        return debt;
+    }
+
+    public int getBalance() {
+        return debt;
+    }
+
+}
+
+
