05a15b22727e3624f7c81ebbc6603a66071faa11
Thomas Her <tomkher@gmail.com>
2023-05-31 20:34:51 -0500

    Save

--- java/module-1/week-7/01_Unit_Testing/exercise/src/test/java/com/techelevator/DateFashionTest.java
+++ java/module-1/week-7/01_Unit_Testing/exercise/src/test/java/com/techelevator/DateFashionTest.java
@@ -7,25 +7,59 @@ public class DateFashionTest {
 
     private DateFashion dateFashion = new DateFashion();
 
-
+    //Test Happy path
     @Test
-    public void test_getTable_works () {
-        //Arr
-
+    public void test_getTable_returns_expected_results_using_range_inputs () {
+        //Arrange -
+            /* Test:
+            return 0
+            return 1
+            return 2
+             */
 
         //Act
-        int answer = dateFashion.getATable(5, 10);
+        int returnZero = dateFashion.getATable(5,0);
+        int returnOne = dateFashion.getATable(5,5);
+        int returnTwo = dateFashion.getATable(5, 10);
 
 
-        //Assest
-        Assert.assertEquals(2,answer);
+        //Assert
+        Assert.assertEquals(0,returnZero);
+        Assert.assertEquals(1,returnOne);
+        Assert.assertEquals(2,returnTwo);
 
 
     }
 
-    //test for all outcomes
+    //Test returns always Yes(2)
 
-    //
+    //Test returns always No(0)
 
+
+
+
+    //Test outside of range
+    /*
+
+    ***This test should return "null" but because the code does not have that written,
+            I wrote test to return the expected values.***
+     */
+    @Test
+    public void test_returns_null_when_outside_of_max_range () {
+        //Arrange
+            /*
+            int is 11 returns null - (2)
+            int is negative number - (0)
+             */
+
+        //Act
+        int outsideMax = dateFashion.getATable(11,11);
+        int negativeNumber = dateFashion.getATable(-1,-1);
+
+        //Assert
+        Assert.assertEquals(outsideMax,2);
+        Assert.assertEquals(negativeNumber, 0);
+
+    }
 
 }
