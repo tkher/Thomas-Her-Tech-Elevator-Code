17347da5b5916fcb23d1df15608c465d66b92095
Thomas Her <tomkher@gmail.com>
2023-06-02 20:47:03 -0500

    save

--- java/module-1/week-7/01_Unit_Testing/exercise/src/test/java/com/techelevator/SameFirstLastTest.java
+++ java/module-1/week-7/01_Unit_Testing/exercise/src/test/java/com/techelevator/SameFirstLastTest.java
@@ -4,24 +4,52 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SameFirstLastTest {
+    private int[] listOne = new int[3];
+    private int[] listTwo = new int[4];
+    private int[] listThree = new int[5];
 
     public SameFirstLast sameFirstLast = new SameFirstLast();
+
+    private boolean actTestOne() {
+        return sameFirstLast.isItTheSame(listOne);
+    }
+
+    private boolean actTestTwo() {
+        return sameFirstLast.isItTheSame(listOne);
+    }
+
+    private boolean actTestThree() {
+        return sameFirstLast.isItTheSame(listOne);
+    }
 
     //Happy path test
     @Test
     public void results_return_true_if_first_and_last_int_are_equal() {
         //Arrange
-        int[] listOne = new int [] {1,2,1,};
-        int[] listTwo = new int [] {1,2,3,1};
-        int[] listThree = new int [] {0,2,0};
+        listOne[0] = 1;
+        listOne[1] = 2;
+        listOne[2] = 1;
+
+        listTwo[0] = 1;
+        listTwo[1] = 2;
+        listTwo[2] = 3;
+        listTwo[3] = 1;
+
+        listThree[0] = 1;
+        listThree[1] = 2;
+        listThree[2] = 3;
+        listThree[3] = 4;
+        listThree[4] = 5;
+
+
         //Act
-        boolean testListOne = sameFirstLast.isItTheSame(listOne);
-        boolean testListTwo = sameFirstLast.isItTheSame(listTwo);
-        boolean testListThree = sameFirstLast.isItTheSame(listThree);
+        actTestOne();
+        actTestTwo();
+        actTestThree();
         //Assert
-        Assert.assertTrue(testListOne);
-        Assert.assertTrue(testListTwo);
-        Assert.assertTrue(testListThree);
+        Assert.assertTrue(actTestOne());
+        Assert.assertTrue(actTestTwo());
+        Assert.assertTrue(actTestThree());
 
     }
 
@@ -31,30 +59,45 @@ public class SameFirstLastTest {
     @Test
     public void results_return_false_if_first_and_last_int_are_not_equal() {
         //Arrange
-        int[] listOne = new int [] {1,2,3,};
-        int[] listTwo = new int [] {1,2,3,4};
-        int[] listThree = new int [] {-1,1,0};
+        listOne[0] = 1;
+        listOne[1] = 2;
+        listOne[2] = 3;
+
+        listTwo[0] = 1;
+        listTwo[1] = 2;
+        listTwo[2] = 3;
+        listTwo[3] = 4;
+
+        listThree[0] = 0;
+        listThree[1] = 1;
+        listThree[2] = 2;
+        listThree[3] = 3;
+        listThree[4] = 4;
         //Act
-        boolean equalIndexOneAndThree = sameFirstLast.isItTheSame(listOne);
-        boolean equalIndexOneAndfour = sameFirstLast.isItTheSame(listTwo);
-        boolean equalZeroValue = sameFirstLast.isItTheSame(listThree);
+        actTestOne();
+        actTestTwo();
+        actTestThree();
         //Assert
-        Assert.assertFalse(equalIndexOneAndThree);
-        Assert.assertTrue(equalIndexOneAndfour);
-        Assert.assertTrue(equalZeroValue);
+        Assert.assertFalse(actTestOne());
+        Assert.assertFalse(actTestTwo());
+        Assert.assertFalse(actTestThree());
+
 
     }
 
 
     //Test if empty array, array of strings, null
     @Test
-    public void results_return_false_if_first_and_last_elements_are_string_empty_null() {
+    public void results_return_false_if_array_is_null() {
         //Arrange
+        int[]nums = null;
 
         //Act
+        boolean testNums = sameFirstLast.isItTheSame(nums);
 
         //Assert
+        Assert.assertFalse(testNums);
+
     }
 
-    //
 }
