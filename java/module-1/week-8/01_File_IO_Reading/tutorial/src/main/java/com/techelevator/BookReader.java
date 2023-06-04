07ada3a395911a80aa73b3937d837d96b17eb8c2
Thomas Her <tomkher@gmail.com>
2023-06-04 14:17:20 -0500

    Save

--- java/module-1/week-8/01_File_IO_Reading/tutorial/src/main/java/com/techelevator/BookReader.java
+++ java/module-1/week-8/01_File_IO_Reading/tutorial/src/main/java/com/techelevator/BookReader.java
@@ -2,6 +2,7 @@ package com.techelevator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class BookReader {
@@ -27,6 +28,36 @@ public class BookReader {
         /*
         Step 2: Step Two: Open the book file and handle errors
          */
+        //Create a file object using the path
+        File bookFile = new File(filePath);
 
+        //initialize a variable to hold the count of lines
+        int lineCount = 0; //count of lines between the start and end markers
+
+        //initialize variable to remove header
+        boolean inBookText = false;
+
+        //open the file
+        try (Scanner fileInput = new Scanner(bookFile)) {
+            while (fileInput.hasNextLine()) {
+                String lineOfText = fileInput.nextLine();
+                //Skip the header information before book content - turns true after BEGIN_MARKER
+                if (lineOfText.startsWith(BEGIN_MARKER)) {
+                    inBookText = true;
+                }
+                if (lineOfText.startsWith(END_MARKER)) {
+                    break;
+                }
+                if (inBookText) {
+                    lineCount++;
+                    System.out.println(lineCount + ": " + lineOfText);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            //Could not find the file at the specified path
+            System.out.println("The file was not found: " + bookFile.getAbsolutePath());
+        }
+        System.out.println("Found " + lineCount + " lines of text in " + filePath);
     }
 }
