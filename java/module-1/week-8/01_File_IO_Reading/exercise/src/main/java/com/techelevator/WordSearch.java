4b7825098d9bdb208f44227d360c5ea45a65daa5
Thomas Her <tomkher@gmail.com>
2023-06-05 21:11:57 -0500

    save

--- java/module-1/week-8/01_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ java/module-1/week-8/01_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class WordSearch {
@@ -15,7 +17,21 @@ public class WordSearch {
 	public void run() {
 		/* Your code goes here */
 
+		//Step 1 - Prompt user for file input
+		System.out.println("Enter path to the book file: ");
+		String filePath = userInput.nextLine();
 
+		File bookFile = new File(filePath);
+
+		try (Scanner fileInput = new Scanner(bookFile)) {
+			while (fileInput.hasNextLine()) {
+				String lineOfText = fileInput.nextLine();
+			}
+		}
+	 catch (FileNotFoundException e) {
+		 System.out.println("The file was not found:" + bookFile.getAbsolutePath());
+	 }
 	}
+
 
 }
