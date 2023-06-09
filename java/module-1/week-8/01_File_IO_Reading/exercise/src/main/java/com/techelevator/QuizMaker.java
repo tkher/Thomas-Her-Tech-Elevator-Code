5f2a1c08abeb43d68f20aa5de782a64f91cc470d
Thomas Her <tomkher@gmail.com>
2023-06-08 22:03:44 -0500

    Finished

--- java/module-1/week-8/01_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
+++ java/module-1/week-8/01_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class QuizMaker {
@@ -14,8 +16,29 @@ public class QuizMaker {
 
 	public void run() {
 		/* Your code goes here */
+		//Initialize variables
+
+		//Ask user to input quiz file
+		System.out.println("Enter the fully qualified name of the file to read in for quiz questions");
+		String quizFileLocation = userInput.nextLine();
+
+		//File object to store file
+		File quizFile = new File(quizFileLocation);
 
 
+		//Display the first question
+		try (Scanner inputOfQuizFile = new Scanner(quizFile)) {
+			while (inputOfQuizFile.hasNextLine()) {
+				String lineOfText = inputOfQuizFile.nextLine();
+				String[] splitUpQuestion = lineOfText.split("\\|");
+//				int delimiter = lineOfText.lastIndexOf('?');
+//				System.out.println(lineOfText.substring(0,delimiter + 1));
+				System.out.println(splitUpQuestion[0]);
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("File not found: " + quizFileLocation);
+		}
 	}
 
 }
