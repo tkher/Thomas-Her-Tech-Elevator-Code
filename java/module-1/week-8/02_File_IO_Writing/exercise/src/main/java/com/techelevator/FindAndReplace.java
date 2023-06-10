d57b3f761e0b1c5c983fd964ef27ac50fc20d77a
Thomas Her <tomkher@gmail.com>
2023-06-09 21:53:11 -0500

    FindandReplace Finished

--- java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -2,6 +2,7 @@ package com.techelevator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
@@ -31,23 +32,35 @@ public class FindAndReplace {
         //Ask for the source file - file writing from
         System.out.println("Enter in the source file: ");
         String sourceFileInput = userInput.nextLine();
-            //**Create the file object for the source file path
+        //**Create the file object for the source file path
         File sourceFile = new File(sourceFileInput);
 
         //Ask for destination file - file writing to
         System.out.println("Enter in the destination file: ");
         String destinationFileInput = userInput.nextLine();
-            //**Create the file object for the destination file path
+        //**Create the file object for the destination file path
         File destinationFile = new File(destinationFileInput);
 
         //try-catch block
-        try (Scanner scanSourceFile = new Scanner(sourceFile)) {
+        try (Scanner scanSourceFile = new Scanner(sourceFile);
+             PrintWriter writeDestinationFile = new PrintWriter(destinationFile)) {
+                while (scanSourceFile.hasNextLine()) {
+                    String lineOfText = scanSourceFile.nextLine(); //read every line - proceeds to nextline till false
+                    if (lineOfText.contains(searchWordInput)) {
+                        lineOfText=lineOfText.replace(searchWordInput,replacementWordInput);
+//                        System.out.println(lineOfText);
+                    }
+                    writeDestinationFile.println(lineOfText);
+                }
 
-        } catch (FileNotFoundException e) {
+
+            writeDestinationFile.close();
+        } catch (FileNotFoundException | NullPointerException e) {
             //indicate to user that file is not found and exit
             System.out.println("The file was not found: " + e.getMessage());
+
         }
 
-    }
 
+    }
 }
