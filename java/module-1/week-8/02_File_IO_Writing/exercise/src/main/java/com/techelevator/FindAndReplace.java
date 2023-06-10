7fdfc8c16d8357a9e5c30253fa0e441c33734994
Thomas Her <tomkher@gmail.com>
2023-06-09 20:41:11 -0500

    save

--- java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,5 +1,8 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class FindAndReplace {
@@ -15,6 +18,35 @@ public class FindAndReplace {
     public void run() {
         /* Your code goes here */
 
+        //Initialize variables
+
+        //Ask for search word
+        System.out.println("Please enter in a search word: ");
+        String searchWordInput = userInput.nextLine();
+
+        //Ask for replacement word
+        System.out.println("Please enter in a replacement word: ");
+        String replacementWordInput = userInput.nextLine();
+
+        //Ask for the source file - file writing from
+        System.out.println("Enter in the source file: ");
+        String sourceFileInput = userInput.nextLine();
+            //**Create the file object for the source file path
+        File sourceFile = new File(sourceFileInput);
+
+        //Ask for destination file - file writing to
+        System.out.println("Enter in the destination file: ");
+        String destinationFileInput = userInput.nextLine();
+            //**Create the file object for the destination file path
+        File destinationFile = new File(destinationFileInput);
+
+        //try-catch block
+        try (Scanner scanSourceFile = new Scanner(sourceFile)) {
+
+        } catch (FileNotFoundException e) {
+            //indicate to user that file is not found and exit
+            System.out.println("The file was not found: " + e.getMessage());
+        }
 
     }
 
