10b617e4d7152319a9ecf40fc0a6621c201e550b
Thomas Her <tomkher@gmail.com>
2023-06-20 22:57:45 -0500

    submission

--- java/module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ java/module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -9,28 +9,39 @@ public class DecimalToBinary {
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter in a series of decimal values (separated by spaces): ");
         String userInput = input.nextLine();
-        String[] listOfuserInput = userInput.split(" ");
-        int decimalValues = 0;
+//        String[] listOfuserInput = userInput.split(" ");
+        int decimalNumber = Integer.parseInt(userInput);
 
-        System.out.print(listOfuserInput[0] + " in binary is ");
+//        System.out.print(listOfuserInput[0] + " in binary is ");
+        //Grab first input value
+//        for (int i = 0; i <= listOfuserInput.length; i++) {
+//            decimalNumber = Integer.parseInt(listOfuserInput[i]);
 
-        for (int i = 0; i <= listOfuserInput.length; i++) {
-            decimalValues = Integer.parseInt(listOfuserInput[i]);
-            for (int a = 0; a <= decimalValues; a ++) {
-                if (decimalValues % 2 == 0) {
-                    decimalValues = decimalValues / 2;
-                    System.out.print("0");
-                } else {
-                    System.out.println("");
-                }
+
+        //determine size of binary number to create array
+        int arrayCounter = 0;
+        int newDecimalNumber = 0;
+        while (decimalNumber % 2 == 0) {
+            decimalNumber = decimalNumber / 2;
+            arrayCounter++;
+        }
+
+        //create Array to hold binary number, needs to add 1 for remainder value
+        int[] binaryNumberList = new int[arrayCounter + 1];
+
+        //loop through decimalNumber to till reaches remainder
+        for (int b = 0; b < binaryNumberList.length; b++) {
+            if (decimalNumber / 2 == 0) {
+                binaryNumberList[b] = (decimalNumber % 2);
+                decimalNumber = decimalNumber / 2;
             }
 
-
-            //for (int i = 0; i <listOfValues.length; i++) {
-            //	listOfValues[i] =
-            //}
-
+            //print out array via loop
+            for (int c = 0; c < binaryNumberList.length; c++)
+                System.out.println(binaryNumberList[c]);
         }
 
     }
 }
+
+
