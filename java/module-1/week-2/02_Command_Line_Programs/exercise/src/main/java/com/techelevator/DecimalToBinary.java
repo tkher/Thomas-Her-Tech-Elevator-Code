9330c71aae8af1a3a8b566046bac0c1fc83b6d1c
Thomas Her <tomkher@gmail.com>
2023-06-20 21:31:27 -0500

    save

--- java/module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ java/module-1/week-2/02_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class DecimalToBinary {
@@ -7,14 +8,29 @@ public class DecimalToBinary {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter in a series of decimal values (separated by spaces): ");
-        //String decimalValues = input.nextLine();
+        String userInput = input.nextLine();
+        String[] listOfuserInput = userInput.split(" ");
+        int decimalValues = 0;
 
-        //String[] listOfValues = decimalValues.split(" ");
+        System.out.print(listOfuserInput[0] + " in binary is ");
 
-        //for (int i = 0; i <listOfValues.length; i++) {
-        //	listOfValues[i] =
-        //}
+        for (int i = 0; i <= listOfuserInput.length; i++) {
+            decimalValues = Integer.parseInt(listOfuserInput[i]);
+            for (int a = 0; a <= decimalValues; a ++) {
+                if (decimalValues % 2 == 0) {
+                    decimalValues = decimalValues / 2;
+                    System.out.print("0");
+                } else {
+                    System.out.println("");
+                }
+            }
+
+
+            //for (int i = 0; i <listOfValues.length; i++) {
+            //	listOfValues[i] =
+            //}
+
+        }
 
     }
-
 }
