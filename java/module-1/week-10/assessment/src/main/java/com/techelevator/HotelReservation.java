a42ee6f0b2c6d653fd9deea397f708858a0bed19
Thomas Her <tomkher@gmail.com>
2023-06-22 19:46:40 -0500

    Final Submission

--- java/module-1/week-10/assessment/src/main/java/com/techelevator/HotelReservation.java
+++ java/module-1/week-10/assessment/src/main/java/com/techelevator/HotelReservation.java
@@ -1,8 +1,66 @@
 package com.techelevator;
 
 public class HotelReservation {
-    /*
-    Fill in the class details here...
-     */
+    private String name;
+    private int numberOfNights;
+    private int nightlyRate;
+    private int estimatedTotal;
+
+    //Constructor
+    public HotelReservation(String name, int numberOfNights, int nightlyRate) {
+        this.name = name;
+        this.numberOfNights = numberOfNights;
+        this.nightlyRate = nightlyRate;
+    }
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public int getNightlyRate() {
+        return nightlyRate;
+    }
+
+    public void setNightlyRate(int nightlyRate) {
+        this.nightlyRate = nightlyRate;
+    }
+
+    public int getEstimatedTotal() {
+        return numberOfNights * nightlyRate;
+    }
+
+    //methods
+    public int getActualTotal(boolean requiresCleaning, boolean usedMinibar) {
+        int finalBill = getEstimatedTotal();
+        int cleaningFee = 25;
+        if(usedMinibar) {
+            finalBill += 15;
+            cleaningFee = cleaningFee * 2;
+        }
+        if(requiresCleaning) {
+            finalBill += cleaningFee;
+        }
+        return finalBill;
+    }
+
+
+    //toString
+    @Override
+    public String toString() {
+        return name + ":" + getEstimatedTotal();
+    }
 
 }
