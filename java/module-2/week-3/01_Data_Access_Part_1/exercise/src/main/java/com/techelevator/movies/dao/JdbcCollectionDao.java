ed97c72bab0a1177920c30358b9db2a1198188d5
Thomas Her <tomkher@gmail.com>
2023-07-20 16:46:16 -0500

    save

--- java/module-2/week-3/01_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcCollectionDao.java
+++ java/module-2/week-3/01_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcCollectionDao.java
@@ -2,12 +2,13 @@ package com.techelevator.movies.dao;
 
 import com.techelevator.movies.model.Collection;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
-public class JdbcCollectionDao implements CollectionDao{
+public class JdbcCollectionDao implements CollectionDao {
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -19,20 +20,46 @@ public class JdbcCollectionDao implements CollectionDao{
     public List<Collection> getCollections() {
         List<Collection> collections = new ArrayList<>();
 
-        String sql = SELECT
+        String sql = "SELECT * " +
+                "FROM collection;";
 
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql,collections);
 
+        if(results.next()) {
+            collections = mapRowToCollection((results));
+        }
 
         return collections;
     }
 
     @Override
     public Collection getCollectionById(int id) {
-        return new Collection(-1, "Not implemented yet");
+        Collection collectionsId = null;
+
+        String sql = "SELECT collection_id" +
+                "FROM collection " +
+                "WHERE collection_id = ?;";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id;
+
+        if (results.next()) {
+            collectionsId = mapRowToCollection(results);
+        }
+            return collectionsId;
     }
 
-    @Override
-    public List<Collection> getCollectionsByName(String name, boolean useWildCard) {
-        return null;
+        @Override
+        public List<Collection> getCollectionsByName (String name,boolean useWildCard){
+            return null;
+        }
     }
+
+    private Collection mapRowToCollection(SqlRowSet rowSet) {
+        Collection collection = new Collection();
+        collection.setId(rowSet.getInt("collection_id"));
+        collection.setName(rowSet.getString("collection_name"));
+        return collection;
+    }
+
 }
+
