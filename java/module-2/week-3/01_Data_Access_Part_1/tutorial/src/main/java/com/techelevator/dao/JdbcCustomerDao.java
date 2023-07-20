a225b2f78dc7a544225813706bd775609fb04144
Thomas Her <tomkher@gmail.com>
2023-07-19 20:49:51 -0500

    complete

--- java/module-2/week-3/01_Data_Access_Part_1/tutorial/src/main/java/com/techelevator/dao/JdbcCustomerDao.java
+++ java/module-2/week-3/01_Data_Access_Part_1/tutorial/src/main/java/com/techelevator/dao/JdbcCustomerDao.java
@@ -17,6 +17,22 @@ public class JdbcCustomerDao implements CustomerDao {
     }
 
     // Step Four: Add a new DAO method
+    @Override
+    public Customer getCustomerById(int customerId) {
+        Customer customer = null;
+
+        String sql= "SELECT customer_id, first_name, last_name, street_address, city, phone_number, " +
+                "email_address, email_offers " +
+                "FROM customer " +
+                "WHERE customer_id = ?;";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, customerId);
+
+        if (results.next()) {
+            customer = mapRowToCustomer(results);
+        }
+        return customer;
+    }
 
 
     @Override
