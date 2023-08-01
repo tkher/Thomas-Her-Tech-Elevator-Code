729c1765cac5d71cb847c2ccbe5b65a6fb0fe7fc
Thomas Her <tomkher@gmail.com>
2023-07-31 20:09:04 -0500

    save

--- java/module-2/week-5/mid-module-project/src/main/java/com/techelevator/ssgeek/dao/JdbcCustomerDao.java
+++ java/module-2/week-5/mid-module-project/src/main/java/com/techelevator/ssgeek/dao/JdbcCustomerDao.java
@@ -1,23 +1,56 @@
 package com.techelevator.ssgeek.dao;
 
+import com.techelevator.ssgeek.exception.DaoException;
 import com.techelevator.ssgeek.model.Customer;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 public class JdbcCustomerDao implements CustomerDao {
 
-public Customer mapValueToCustomer (SqlRowSet results);
+private final JdbcTemplate jdbcTemplate;
+
+public JdbcCustomerDao(DataSource dataSource) {this.jdbcTemplate = new JdbcTemplate(dataSource);}
+
+public Customer mapValueToRowSet (SqlRowSet results) {
     Customer customer = new Customer();
     customer.setCustomerId(results.getInt("customer_id"));
-
+    customer.setName(results.getString("name"));
+    customer.setStreetAddress1(results.getString("street_address1"));
+    customer.setStreetAddress2(results.getString("street_address2"));
+    customer.setCity(results.getString("city"));
+    customer.setState(results.getString("state"));
+    customer.setZipCode(results.getString("zip_code"));
+    return customer;
+}
 
     @Override
 
     public Customer getCustomerById(int customerId) {
+        Customer customer = null;
 
-        return null;
-    }
+        String sql = "SELECT * " +
+                "FROM customer " +
+                "WHERE customer_id = ?;";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, customerId);
+
+        try {
+            if (results.next()) {
+                customer = mapValueToRowSet(results);
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Cannot connect to Datebase", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new DaoException("Data Violation", e);
+        }
+            return customer;
+        }
+
 
     public List<Customer> getCustomers() {
         return null;
