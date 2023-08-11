07d80a75c7ee8284490860dbde1c875c07878e37
Thomas Her <tomkher@gmail.com>
2023-08-10 20:18:33 -0500

    save

--- java/module-2/week-6/01_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
+++ java/module-2/week-6/01_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
@@ -7,27 +7,25 @@ import com.techelevator.auctions.model.Auction;
 public class AuctionService {
 
     public static String API_BASE_URL = "http://localhost:3000/auctions/";
+    public static String API_BASE_SEARCH_URL = "http://localhost:3000/auctions?";
     private RestTemplate restTemplate = new RestTemplate();
 
 
+
     public Auction[] getAllAuctions() {
-        // call api here
-        return null;
+        return restTemplate.getForObject(API_BASE_URL, Auction[].class);
     }
 
     public Auction getAuction(int id) {
-        // call api here
-        return null;
+        return restTemplate.getForObject(API_BASE_URL + id, Auction.class);
     }
 
     public Auction[] getAuctionsMatchingTitle(String title) {
-        // call api here
-        return null;
+        return restTemplate.getForObject(API_BASE_SEARCH_URL + "title_like=" + title, Auction[].class);
     }
 
     public Auction[] getAuctionsAtOrBelowPrice(double price) {
-        // call api here
-        return null;
+        return restTemplate.getForObject(API_BASE_SEARCH_URL + "currentBid_lte<=" + price, Auction[].class);
     }
 
 }
