4dfd68b9f2d0be2387a06ce8110338dba37c4015
Thomas Her <tomkher@gmail.com>
2023-08-18 20:11:58 -0500

    save

--- java/module-2/week-7/01_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ java/module-2/week-7/01_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,7 +2,12 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/auctions")
@@ -13,5 +18,38 @@ public class AuctionController {
     public AuctionController() {
         this.auctionDao = new MemoryAuctionDao();
     }
+
+    //private List<Auction> auctions = new ArrayList<>();
+
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public List<Auction> list() {
+        return auctionDao.getAuctions();
+    }
+
+    @RequestMapping(path = "{id}", method = RequestMethod.GET)
+    public Auction get(@PathVariable int id) {
+        return auctionDao.getAuctionById(id);
+
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public Auction create(@RequestBody Auction auction) {
+//        if (auction !=null) {
+           return auctionDao.createAuction(auction);
+        }
+
+//    @RequestMapping(path = "", method = RequestMethod.GET)
+//    public List<Auction> auctionsByTitle(@RequestParam String title_like)
+//                                         @RequestParam String limit)
+//                                         {
+//        if (title_like != null) {
+//            return auctionDao.getAuctionsByTitle(title_like);
+//        } else {
+//            return auctionDao.getAuctions();
+//        }
+//    }
+
+
+
 
 }
