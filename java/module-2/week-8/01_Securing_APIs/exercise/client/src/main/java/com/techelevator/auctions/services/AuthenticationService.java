4b4a98619637ad16fe90b9bc38373c69454e2d13
Thomas Her <tomkher@gmail.com>
2023-08-25 18:46:48 -0500

    Client finished

--- java/module-2/week-8/01_Securing_APIs/exercise/client/src/main/java/com/techelevator/auctions/services/AuthenticationService.java
+++ java/module-2/week-8/01_Securing_APIs/exercise/client/src/main/java/com/techelevator/auctions/services/AuthenticationService.java
@@ -1,11 +1,14 @@
 package com.techelevator.auctions.services;
 
 import com.techelevator.auctions.model.CredentialsDto;
+import com.techelevator.auctions.model.TokenDto;
 import com.techelevator.util.BasicLogger;
 import org.springframework.http.*;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 
 public class AuthenticationService {
@@ -22,7 +25,11 @@ public class AuthenticationService {
         HttpEntity<CredentialsDto> entity = new HttpEntity<>(credentialsDto, headers);
         String token = null;
         try {
-            // Add code here to send the request to the API and get the token from the response.
+            ResponseEntity<TokenDto> response = restTemplate.exchange(API_BASE_URL + "login", HttpMethod.POST, entity, TokenDto.class);
+            TokenDto body = response.getBody();
+            if (body !=null) {
+                token = body.getToken();
+            }
         } catch (RestClientResponseException | ResourceAccessException e) {
             BasicLogger.log(e.getMessage());
         }
--- java/module-2/week-8/01_Securing_APIs/tutorial/client/src/main/java/com/techelevator/locations/services/LocationService.java
+++ java/module-2/week-8/01_Securing_APIs/tutorial/client/src/main/java/com/techelevator/locations/services/LocationService.java
@@ -21,7 +21,19 @@ public class LocationService {
     public Location[] getAll() {
         Location[] locations = null;
         try {
-            locations = restTemplate.getForObject(API_BASE_URL, Location[].class);
+            //step 1 - Create HTTP headers in order to pass the authentication code
+                //Refactored into makeAuthEntity()
+//            HttpHeaders httpHeaders = new HttpHeaders();
+//            httpHeaders.setBearerAuth(authToken);
+
+            //step 2 - We need to use an exchange() method in order for us to send the get request and recieve the response/response body
+            //we do so by creating http entity
+//            HttpEntity<Void> entity = new HttpEntity<>(httpHeaders); // httpEntity takes the headers as the parameter
+            ResponseEntity<Location[]> response = restTemplate.exchange(API_BASE_URL, HttpMethod.GET, makeAuthEntity(), Location[].class);
+
+            locations=response.getBody(); //reponse method replaces previous get methoed below.
+
+            //locations = restTemplate.getForObject(API_BASE_URL, Location[].class); <-- old get method
         } catch (RestClientResponseException | ResourceAccessException e) {
             BasicLogger.log(e.getMessage());
         }
@@ -31,7 +43,14 @@ public class LocationService {
     public Location getOne(int id) {
         Location location = null;
         try {
-            location = restTemplate.getForObject(API_BASE_URL + id, Location.class);
+            //Step two - get one
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setBearerAuth(authToken);
+
+            HttpEntity<Void> entity = new HttpEntity<>(httpHeaders);
+            ResponseEntity<Location> response = restTemplate.exchange(API_BASE_URL + id,HttpMethod.GET,makeAuthEntity(),Location.class);
+
+            location = response.getBody();
         } catch (RestClientResponseException | ResourceAccessException e) {
             BasicLogger.log(e.getMessage());
         }
@@ -66,7 +85,7 @@ public class LocationService {
     public boolean delete(int id) {
         boolean success = false;
         try {
-            restTemplate.delete(API_BASE_URL + id);
+            restTemplate.exchange(API_BASE_URL + id, HttpMethod.DELETE, makeAuthEntity(), Void.class);
             success = true;
         } catch (RestClientResponseException | ResourceAccessException e) {
             BasicLogger.log(e.getMessage());
@@ -77,7 +96,14 @@ public class LocationService {
     private HttpEntity<Location> makeLocationEntity(Location location) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(authToken);
         return new HttpEntity<>(location, headers);
+    }
+
+    private HttpEntity<Void> makeAuthEntity() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setBearerAuth(authToken);
+        return new HttpEntity<>(httpHeaders);
     }
 
 }
