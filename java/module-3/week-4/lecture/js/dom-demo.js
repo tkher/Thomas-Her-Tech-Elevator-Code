08573924cdcad09dc6e540364a4a9ce0be9d077b
Daniel Commins <daniel.commins@techelevator.com>
2023-10-10 15:00:22 -0700

    m3-w4 lecture

---
+++ java/module-3/week-4/lecture/js/dom-demo.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', onDomLoad);
+
+/*
+function onDomLoad() {
+  const introEl = document.getElementById('intro');
+  introEl.innerText = 'I dedicate this page to my cat';
+  console.log(`introEl is: ${introEl}`);
+  console.log(`introEl.innterText is: ${introEl.innerText}`);
+  console.log(`introEl.id is: ${introEl.id}`);
+}
+
+const contentEl = document.querySelector('.content');
+console.log(`contentEl is: ${contentEl}`);
+console.log(`contentEl.innerText is: ${contentEl.innerText}`);
+
+const contentElements = document.querySelectorAll('.content');
+console.log(`contentElements is: ${contentElements}`);
+
+for(let i = 0; i < contentElements.length; i++){
+  const eachEl = contentElements[i];
+  eachEl.style.color = 'blue';
+  console.log(`eachEl.innerText is: ${eachEl.innerText}`);
+}
+
+const newLiEl = document.createElement('li');
+newLiEl.innerText = "Hey look, I'm adding a new <li>!";
+newLiEl.setAttribute('class', 'importantStuff');
+newLiEl.classList.add('moreImportantStuff');
+//newLiEl.classList.remove('importantStuff');
+
+const parentEl = document.getElementById('theList');
+//parentEl.appendChild(newLiEl);
+parentEl.insertAdjacentElement('beforebegin', newLiEl);
+console.log();
+*/
