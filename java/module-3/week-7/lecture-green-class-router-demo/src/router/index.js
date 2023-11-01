ba59d853c9b92ce0ea2b5041762c5c0c47c76155
Daniel Commins <daniel.commins@techelevator.com>
2023-11-01 09:39:20 -0700

    m3-w7-d1 vue store & router lecture code

---
+++ java/module-3/week-7/lecture-green-class-router-demo/src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ProductsView from '../views/ProductsView.vue'
+import ProductsDetailView from '../views/ProductDetailView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: ProductsView
+    },
+    {
+      path: '/products/:id',
+      name: 'productsDetail',
+      component: ProductsDetailView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ java/module-3/week-7/lecture-green-class-router-demo/src/views/ProductDetailView.vue
@@ -0,0 +1,34 @@
+<template lang="">
+  <div>
+    <p>{{ propsData }}</p>
+    <p>{{ productData }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['prodData'],
+  data(){
+    return {
+      productData: 'nothing yet :('
+    }
+  },
+  // created() is called when this component loads
+  created(){
+    // If we can get the product id from the URL,
+    // we can get the product information from the...
+    // VueX Store
+
+    const productId = parseInt(this.$route.params.id);
+    const products = this.$store.state.allProducts;
+
+    // Find product by product id...
+    this.productData = products.find( (eachProduct) => {
+      return eachProduct.productId === productId;
+    });
+  }
+}
+</script>
+<style lang="">
+  
+</style>
