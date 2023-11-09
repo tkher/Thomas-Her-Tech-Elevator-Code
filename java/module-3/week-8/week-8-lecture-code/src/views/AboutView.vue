bbe8f3c3bacb62429c63d1e08b417283639ee5e8
Daniel Commins <daniel.commins@techelevator.com>
2023-11-09 15:00:22 -0800

    m3-week-8 lecture code

---
+++ java/module-3/week-8/week-8-lecture-code/src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>About View</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.about {
+  padding-top: 20px;
+  display: flex;
+  justify-content: center;
+}
+
+input {
+  margin-left: 20px;
+}
+
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
